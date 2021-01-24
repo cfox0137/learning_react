4333815a3ff4a570b168e425f304ffb9ded816d2
Kimczym <cfox0137@gmail.com>
2021-01-24 17:22:28 +0100

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,29 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
-
-  const [blogs, setBlogs] = useState([
-    { title: "title1", body: "lorem", author: "author1", id: 1 },
-    { title: "title2", body: "lorem", author: "author2", id: 2 },
-    { title: "title3", body: "lorem", author: "author3", id: 3 },
-  ]);
-
-  const handleDelete = (id) => {
-        
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    console.log("testy")
+    fetch("http://localhost:8000/blogs").then((res) => {
+      return res.json().then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+    });
   }, []);
- 
 
   return (
     <div className="home">
-      <BlogList const blogs={blogs} title = "What a nice title" handleDelete={handleDelete} />
-      <BlogList const blogs={blogs.filter((blog)=>(
-        blog.author === "author2"
-      ))} title = "Other blogs" handleDelete={handleDelete}/>
-
+      {isPending && <div>Loading</div>}
+      {blogs && <BlogList const blogs={blogs} title="What a nice title" />}
+      {blogs && (
+        <BlogList
+          const
+          blogs={blogs.filter((blog) => blog.author === "author2")}
+          title="Other blogs"
+        />
+      )}
     </div>
   );
 };
